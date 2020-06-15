b67179cbc60b8e7725b16b36f5ac1b3100f3fcab
Marc-Antoine Ruel <hello@marcantoineruel.com>
2020-06-15 16:21:54 -0400

    add deploy button in sanity

--- sanity/src/dashboardConfig.js
+++ sanity/src/dashboardConfig.js
@@ -1,20 +1,18 @@
 export default {
   widgets: [
-    {name: 'project-users'},
-    {
+      {
       name: 'netlify',
       options: {
-        title: 'netlify Server',
+        title: 'My Netlify deploys',
         sites: [
           {
             title: 'Site web',
             apiId: '15a6d34b-9cb3-4242-bc6b-c0a40b2dc14b',
             buildHookId: '5d7109dbe8f19d0d35a099b9',
-            name: 'roi-du-boudin',
+            name: 'sanity-gatsby-blog-20-studio',
           }
         ]
       }
-    },
-    {name: 'project-info'}
+    }
   ]
 }
